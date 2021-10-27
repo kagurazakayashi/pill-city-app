d6bbb4357fb7cbece8a15c2623bb018ed3ba7ec5
神楽坂雅詩 <cyscxchope@gmail.com>
2021-10-27 19:30:02 +0800

    用户信息数据模型增加昵称字段
    author/display_name，可为空，为空时用 author/id

--- lib/data_moudle/data_author.dart
+++ lib/data_moudle/data_author.dart
@@ -1,13 +1,16 @@
 class DataAuthor {
   String id = ""; // "yashi"
   int created_at_seconds = -1; // 1632125699
-  String avatar_url = ""; // "https://pill-city.s3.us-west-2.amazonaws.com/avatars/yashi-1632125933.png"
+  String avatar_url =
+      ""; // "https://pill-city.s3.us-west-2.amazonaws.com/avatars/yashi-1632125933.png"
   String profile_pic = ""; // "pill4.png"
+  String display_name = ""; // "史詩級巨大 KT"
 
-  DataAuthor(Map<String,dynamic> data) {
+  DataAuthor(Map<String, dynamic> data) {
     id = data['id'];
     created_at_seconds = data['created_at_seconds'].toInt();
     avatar_url = data['avatar_url'];
     profile_pic = data['profile_pic'];
+    display_name = data['display_name'] ?? id;
   }
 }
